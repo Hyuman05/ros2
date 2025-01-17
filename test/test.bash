38e8416f24867cf621dc188f50e42a1fa7e5e3e3
hyuta sasaki <s23C1065UR@s.chibakoudai.jp>
2025-01-17 15:26:51 +0900

    addc

--- test/test.bash
+++ test/test.bash
@@ -9,7 +9,7 @@ ros2 launch mypkg translate_question.launch.py &
 sleep 10
 
 # Test 1: Check translation for "こんにちは"
-result_hello=$(ros2 topic echo /translation_topic --once | grep -o 'Hello')
+result_hello=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Hello')
 if [ "$result_hello" == "Hello" ]; then
     echo "Test Passed: 'こんにちは' was correctly translated to 'Hello'"
 else
@@ -17,7 +17,7 @@ else
 fi
 
 # Test 2: Check translation for "ありがとう"
-result_thanks=$(ros2 topic echo /translation_topic --once | grep -o 'Thank you')
+result_thanks=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Thank you')
 if [ "$result_thanks" == "Thank you" ]; then
     echo "Test Passed: 'ありがとう' was correctly translated to 'Thank you'"
 else
@@ -25,7 +25,7 @@ else
 fi
 
 # Test 3: Check translation for "さようなら"
-result_goodbye=$(ros2 topic echo /translation_topic --once | grep -o 'Goodbye')
+result_goodbye=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Goodbye')
 if [ "$result_goodbye" == "Goodbye" ]; then
     echo "Test Passed: 'さようなら' was correctly translated to 'Goodbye'"
 else
@@ -33,7 +33,7 @@ else
 fi
 
 # Test 4: Check translation for "いぬ"
-result_dog=$(ros2 topic echo /translation_topic --once | grep -o 'Dog')
+result_dog=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Dog')
 if [ "$result_dog" == "Dog" ]; then
     echo "Test Passed: 'いぬ' was correctly translated to 'Dog'"
 else
@@ -41,7 +41,7 @@ else
 fi
 
 # Test 5: Check translation for "ねこ"
-result_cat=$(ros2 topic echo /translation_topic --once | grep -o 'Cat')
+result_cat=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Cat')
 if [ "$result_cat" == "Cat" ]; then
     echo "Test Passed: 'ねこ' was correctly translated to 'Cat'"
 else
@@ -49,7 +49,7 @@ else
 fi
 
 # Test 6: Check translation for "Dog" (should return 'Unknown')
-result_unknown=$(ros2 topic echo /translation_topic --once | grep -o 'Unknown')
+result_unknown=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Unknown')
 if [ "$result_unknown" == "Unknown" ]; then
     echo "Test Passed: 'Dog' was correctly translated to 'Unknown'"
 else
@@ -57,4 +57,4 @@ else
 fi
 
 # Stop the launch process after testing
-killall ros2
+pkill ros2
