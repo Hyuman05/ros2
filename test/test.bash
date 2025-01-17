9fa9659e0e132138318fd571f7ad22e0d077d852
hyuta sasaki <s23C1065UR@s.chibakoudai.jp>
2025-01-17 15:50:20 +0900

    addc

--- test/test.bash
+++ test/test.bash
@@ -16,7 +16,7 @@ node_pid=$!
 sleep 10
 
 # Test 1: Check translation for "こんにちは"
-result_hello=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Hello')
+result_hello=$(ros2 topic echo -t std_msgs/Trigger/translation_topic --once | grep -o 'Hello')
 if [ "$result_hello" == "Hello" ]; then
     echo "Test Passed: 'こんにちは' was correctly translated to 'Hello'"
 else
@@ -24,7 +24,7 @@ else
 fi
 
 # Test 2: Check translation for "ありがとう"
-result_thanks=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Thank you')
+result_thanks=$(ros2 topic echo -t std_msgs/Trigger/translation_topic --once | grep -o 'Thank you')
 if [ "$result_thanks" == "Thank you" ]; then
     echo "Test Passed: 'ありがとう' was correctly translated to 'Thank you'"
 else
@@ -32,7 +32,7 @@ else
 fi
 
 # Test 3: Check translation for "さようなら"
-result_goodbye=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Goodbye')
+result_goodbye=$(ros2 topic echo -t std_msgs/Trigger/translation_topic --once | grep -o 'Goodbye')
 if [ "$result_goodbye" == "Goodbye" ]; then
     echo "Test Passed: 'さようなら' was correctly translated to 'Goodbye'"
 else
@@ -40,7 +40,7 @@ else
 fi
 
 # Test 4: Check translation for "いぬ"
-result_dog=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Dog')
+result_dog=$(ros2 topic echo -t std_msgs/Trigger/translation_topic --once | grep -o 'Dog')
 if [ "$result_dog" == "Dog" ]; then
     echo "Test Passed: 'いぬ' was correctly translated to 'Dog'"
 else
@@ -48,7 +48,7 @@ else
 fi
 
 # Test 5: Check translation for "ねこ"
-result_cat=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Cat')
+result_cat=$(ros2 topic echo -t std_msgs/Trigger/translation_topic --once | grep -o 'Cat')
 if [ "$result_cat" == "Cat" ]; then
     echo "Test Passed: 'ねこ' was correctly translated to 'Cat'"
 else
@@ -56,7 +56,7 @@ else
 fi
 
 # Test 6: Check translation for "Dog" (should return 'Unknown')
-result_unknown=$(ros2 topic echo -t std_msgs/String/translation_topic --once | grep -o 'Unknown')
+result_unknown=$(ros2 topic echo -t std_msgs/Trigger/translation_topic --once | grep -o 'Unknown')
 if [ "$result_unknown" == "Unknown" ]; then
     echo "Test Passed: 'Dog' was correctly translated to 'Unknown'"
 else
